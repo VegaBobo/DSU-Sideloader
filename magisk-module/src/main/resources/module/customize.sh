90627e0224892f95c3ff6a4d48433ca9e9418684
VegaBobo <vegazstestando@gmail.com>
2022-10-07 17:11:28 -0300

    Update module with prebuilt custom gsid

--- magisk-module/src/main/resources/module/customize.sh
+++ magisk-module/src/main/resources/module/customize.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
-if [[ $API == "32" ]] || [[ $API == "31" ]]; then
-  if [[ $ARCH == "arm64" ]]; then
-    ui_print "- Installing custom gsid binary.."
-    mkdir -p $MODPATH/system/bin
-    mv $MODPATH/bin/Android12/gsid $MODPATH/system/bin/gsid
-    # Minimum allowed for allocation is hardcoded in gsid binary
-    # values below are used by DSU Sideloader UI, nothing else
-    echo "ro.vegabobo.dsusideloader.gsid_min_alloc=0.20" >> $MODPATH/system.prop
-  fi
+function install_gsid() {
+  ui_print "- Installing custom gsid binary..."
+  ui_print "- API Level: $API"
+  ui_print "- Arch: $ARCH"
+  mkdir -p $MODPATH/system/bin
+  cat "$MODPATH/bin-$ARCH/$API/gsid" > $MODPATH/system/bin/gsid
+
+  # Minimum allowed for allocation is hardcoded in gsid binary
+  # values below are used by DSU Sideloader UI, nothing else
+  echo "ro.vegabobo.dsusideloader.gsid_min_alloc=0.20" >> $MODPATH/system.prop
+}
+
+if [[ $API -le "33" && $API -ge "29" ]] &&
+   [[ $ARCH == arm64 || $ARCH == arm ]]
+then
+    install_gsid
 fi
