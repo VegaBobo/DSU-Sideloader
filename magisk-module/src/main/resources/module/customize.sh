c08af8e237758846a4c4280829c0022dbb9f18d0
VegaBobo <vegazstestando@gmail.com>
2022-10-07 23:46:54 -0300

    Improve customize.sh

--- magisk-module/src/main/resources/module/customize.sh
+++ magisk-module/src/main/resources/module/customize.sh
@@ -4,15 +4,20 @@ function install_gsid() {
   ui_print "- API Level: $API"
   ui_print "- Arch: $ARCH"
   mkdir -p $MODPATH/system/bin
-  cat "$MODPATH/bin-$ARCH/$API/gsid" > $MODPATH/system/bin/gsid
+  cat "$MODPATH/bin-$ARCH/$API/gsid" >$MODPATH/system/bin/gsid
 
   # Minimum allowed for allocation is hardcoded in gsid binary
   # values below are used by DSU Sideloader UI, nothing else
-  echo "ro.vegabobo.dsusideloader.gsid_min_alloc=0.20" >> $MODPATH/system.prop
+  echo "ro.vegabobo.dsusideloader.gsid_min_alloc=0.20" >>$MODPATH/system.prop
+}
+
+function clean() {
+  rm -rf $MODPATH/bin-arm
+  rm -rf $MODPATH/bin-arm64
 }
 
 if [[ $API -le "33" && $API -ge "29" ]] &&
-   [[ $ARCH == arm64 || $ARCH == arm ]]
-then
-    install_gsid
+  [[ $ARCH == arm64 || $ARCH == arm ]]; then
+  install_gsid
+  clean
 fi
