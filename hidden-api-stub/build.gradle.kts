e19df092c618abf56b9c990977ee515b67541a39
VegaBobo <vegazstestando@gmail.com>
2023-04-07 01:41:30 -0300

    Update AGP, dependencies and more

--- hidden-api-stub/build.gradle.kts
+++ hidden-api-stub/build.gradle.kts
@@ -11,11 +11,11 @@ android {
         minSdk = 29
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     buildTypes {
         create("miniDebug"){}
